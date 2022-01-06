f49e59a53192f13b5873197d7bb7f85707daf7ce
Vince Howard <vincenguyenhoward@gmail.com>
2022-01-06 13:30:02 -0700

    added procedural corridors and rooms

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod map;
+mod map_builder;
 mod player;
 
 mod prelude {
@@ -6,32 +7,50 @@ mod prelude {
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
+    pub use crate::map_builder::*;
     pub use crate::player::*;
 }
 
 use prelude::*;
 
+pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+
 struct State {
     map: Map,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
-        Self { map: Map::new() }
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+        Self {
+            map: map_builder.map,
+            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+        }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
-
 fn main() -> BError {
+    let dungeon_font = "dungeonfont.png";
     let context = BTermBuilder::simple80x50()
         .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font(dungeon_font, 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, dungeon_font)
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, dungeon_font)
         .build()?;
 
     main_loop(context, State::new())
