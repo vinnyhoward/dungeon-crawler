c80041513dc14c5416aa3e48de409abbd377990b
Vince Howard <vincenguyenhoward@gmail.com>
2022-01-05 22:06:30 -0700

    added boiler plate code as well as tiles

---
+++ src/player.rs
@@ -0,0 +1,22 @@
+use crate::prelude::*;
+
+pub struct Player {
+    pub position: Point,
+}
+
+impl Player {
+    pub fn new(position: Point) -> Self {
+        Self {
+            position,
+        }
+    }
+    pub fn render(&self, ctx: &mut BTerm) {
+        ctx.set(
+            self.position.x,
+            self.position.y,
+            WHITE,
+            BLACK,
+            to_cp437('@'),
+        )
+    }
+}
