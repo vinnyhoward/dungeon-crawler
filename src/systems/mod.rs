b671c525533d124a6051aed92da12c61de44b2ab
Vince Howard <vincenguyenhoward@gmail.com>
2022-01-12 20:26:26 -0700

    adding attack systems

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,14 +1,14 @@
+use crate::prelude::*;
 
-mod end_turn;
-mod entity_render;
-mod hud;
 mod map_render;
-mod movement;
+mod entity_render;
 mod player_input;
 mod random_move;
+mod end_turn;
+mod movement;
+mod hud;
 mod tooltips;
-
-use crate::prelude::*;
+mod combat;
 
 pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
@@ -23,8 +23,9 @@ pub fn build_input_scheduler() -> Schedule {
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
-        .add_system(movement::movement_system())
+        .add_system(combat::combat_system())
         .flush()
+        .add_system(movement::movement_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
@@ -37,8 +38,9 @@ pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(random_move::random_move_system())
         .flush()
-        .add_system(movement::movement_system())
+        .add_system(combat::combat_system())
         .flush()
+        .add_system(movement::movement_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
